269767639d035a6f10e36ca947675b820a3587a4
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-04-09 08:15:13 -0400

    Updated middle image section

--- js/index.js
+++ js/index.js
@@ -51,6 +51,7 @@ navItems[3].textContent = siteContent.nav["nav-item-4"];
 navItems[4].textContent = siteContent.nav["nav-item-5"];
 navItems[5].textContent = siteContent.nav["nav-item-6"];
 
+
 //Update the CTA section
 let ctaText= document.querySelector(".cta h1");
 ctaText.textContent = siteContent.cta["h1"];
@@ -76,3 +77,30 @@ topContentAbout[1].textContent = siteContent["main-content"]["about-h4"];
 
 let topContentTextAbout= document.querySelectorAll(".main-content .top-content p");
 topContentTextAbout[1].textContent = siteContent["main-content"]["about-content"];
+
+
+//Update the middle-img section 
+let middleImage= document.querySelector("#middle-img");
+middleImage.src = siteContent["main-content"]["middle-img-src"];
+
+
+/*
+"main-content": {
+
+  "middle-img-src": "img/mid-page-accent.jpg",
+  "services-h4":"Services",
+  "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+  "product-h4":"Product",
+  "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+  "vision-h4":"Vision",
+  "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+},
+"contact": {
+  "contact-h4" : "Contact",
+  "address" : "123 Way 456 Street Somewhere, USA",
+  "phone" : "1 (888) 888-8888",
+  "email" : "sales@greatidea.io",
+},
+"footer": {
+  "copyright" : "Copyright Great Idea! 2018"
+},*/
